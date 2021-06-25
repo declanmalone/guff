d188673bf06e599be877b15f02a14a3e0e4392af
Declan Malone <idablack@users.sourceforge.net>
2021-06-26 00:03:43 +0100

    Add a little bit of documentation and reorder methods to be a bit more natural.

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,10 @@
-
-
+//! # Grand Unified Finite Field library
+//!
+//! Implements GF(2<sup>x</sup>) for various natural sizes (currently
+//! 8, 16 and 32).
+//! 
+//! 
+//! 
 
 
 use num_traits;
@@ -23,58 +28,28 @@ where T : 'static + Copy
 {}
 
 pub trait GaloisField {
+    /// Natural storage class (u8, u16, u32, etc.) for storing
+    /// elements of the field.
     type E  : FieldStore;
+    /// The next largest natural storage class, eg if E is u8, then EE
+    /// should be U16. Used for:
+    ///
+    ///* storing/returning field polynomial, which is always larger
+    ///  than the largest field element
+    ///
+    ///* storing the result of a non-modular (overflowing) multiply of
+    /// two field elements
     type EE : FieldStore;
 
+    /// The field polynomial with (implicit) high bit stripped off.
     fn poly(&self) -> Self::E;
+    /// Field polynomial in full
     fn full_poly(&self) -> Self::EE;
+    /// These will become associated constants
     fn high_bit() -> Self::E;
     fn order() -> u16;
     fn field_mask() -> Self::E;
 
-    fn div(&self, a : Self::E, b : Self::E) -> Self::E
-    {
-	self.mul(a, self.inv(b))
-    }
-    fn inv(&self, a : Self::E) -> Self::E
-    {
-	let (zero, one)    = (Self::E::zero(),
-			      Self::E::one());
-	let mut u : Self::E = self.poly();
-	let mut v : Self::E = a;
-	let mut z : Self::E = zero;
-	let mut g : Self::E = one;
-	let mut t : Self::E;	// always set before being used
-	// special cases of 1/1 and 1/0
-	if a == zero || a == one { return a }
-	// unroll first loop iteration (knowing initial i >= 0)
-	// rustc can't determine that i is always positive here,
-	// though, so we have to try_into()
-	let mut i : u32 = 1 + v.leading_zeros();
-	u = u ^ v << i as usize;
-	z = z ^ g << i as usize;
-	while u != one {
-	    // in C: i=size_in_bits(u) - size_in_bits(v)
-	    // size_in_bits is order - leading zeroes
-	    // so we get i = (order - clz(u)) - (order - clz(v))
-	    // the order cancels and we get:
-	    // i = clz(v) - clz(u)
-	    // further changed this to make i unsigned always
-	    // i = v.leading_zeros() - u.leading_zeros();
-	    if u.leading_zeros() > v.leading_zeros() {
-		t=u; u=v; v=t;
-		t=z; z=g; g=t;
-		//		    i = u.leading_zeros() - v.leading_zeros();
-	    } else {
-		//		    i = v.leading_zeros() - u.leading_zeros();
-	    }
-	    i = v.leading_zeros() - u.leading_zeros();
-	    u = u ^ v << i as usize;
-	    z = z ^ g << i as usize;
-	}
-	z
-    }
-    
     fn add(&self, a : Self::E, b : Self::E) -> Self::E
     {
 	a ^ b
@@ -117,6 +92,50 @@ pub trait GaloisField {
 	// unreachable!()
     }
 
+    fn div(&self, a : Self::E, b : Self::E) -> Self::E
+    {
+	self.mul(a, self.inv(b))
+    }
+    
+    fn inv(&self, a : Self::E) -> Self::E
+    {
+	let (zero, one)    = (Self::E::zero(),
+			      Self::E::one());
+	let mut u : Self::E = self.poly();
+	let mut v : Self::E = a;
+	let mut z : Self::E = zero;
+	let mut g : Self::E = one;
+	let mut t : Self::E;	// always set before being used
+	// special cases of 1/1 and 1/0
+	if a == zero || a == one { return a }
+	// unroll first loop iteration (knowing initial i >= 0)
+	// rustc can't determine that i is always positive here,
+	// though, so we have to try_into()
+	let mut i : u32 = 1 + v.leading_zeros();
+	u = u ^ v << i as usize;
+	z = z ^ g << i as usize;
+	while u != one {
+	    // in C: i=size_in_bits(u) - size_in_bits(v)
+	    // size_in_bits is order - leading zeroes
+	    // so we get i = (order - clz(u)) - (order - clz(v))
+	    // the order cancels and we get:
+	    // i = clz(v) - clz(u)
+	    // further changed this to make i unsigned always
+	    // i = v.leading_zeros() - u.leading_zeros();
+	    if u.leading_zeros() > v.leading_zeros() {
+		t=u; u=v; v=t;
+		t=z; z=g; g=t;
+		//		    i = u.leading_zeros() - v.leading_zeros();
+	    } else {
+		//		    i = v.leading_zeros() - u.leading_zeros();
+	    }
+	    i = v.leading_zeros() - u.leading_zeros();
+	    u = u ^ v << i as usize;
+	    z = z ^ g << i as usize;
+	}
+	z
+    }
+    
     fn pow(&self, a : Self::E, b : Self::EE) -> Self::E {
 	let mut result : Self::E = a;
 	let zero : Self::EE     = num::zero();
@@ -151,7 +170,7 @@ pub trait GaloisField {
 }
 
 
-
+struct F8;
 
 
 
